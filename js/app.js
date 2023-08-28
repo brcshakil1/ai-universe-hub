94be6c145fc5da46472faec341148068978b3487
brcshakil1 <brcshakil72@gmail.com>
2023-08-28 18:52:58 +0600

    card name added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const loadData = async () => {
 const showAiTools = (aiTools) => {
     const aiToolsContainer = document.getElementById('ai-tools-container');
     aiTools.forEach(tool => {
+        console.log(tool)
         const aiCard = document.createElement('div');
         aiCard.classList.add('card', 'card-compact', 'w-96', 'bg-base-100')
         aiCard.innerHTML = `
@@ -18,13 +19,17 @@ const showAiTools = (aiTools) => {
               />
             </figure>
             <div class="card-body">
-              <h2 class="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-              </div>
+              <h2 class="card-title">Features</h2>
+              <ol id="features-container">
+                
+              </ol>
             </div>
-        `; 
+            <div class=" h-[1px] bg-gray-500 mx-4"></div>
+            <div class="p-4 space-y-4">
+                <h3 class="text-2xl font-semibold">${tool.name}</h3>
+                <p class="text-base text-gray-500"><i class="fa-solid fa-calendar-days"></i> <span class="pl-1">${tool.published_in}</span></p>
+            </div>
+        `;
         aiToolsContainer.appendChild(aiCard)
     })
 }
